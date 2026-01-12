8a0924dfb7db5a690e3cf27489f6c33e10e2878b
Dominic Giarrusso <98626228+DomGiarrusso@users.noreply.github.com>
2026-01-12 00:03:11 -0500

    feat: initial navbar

--- src/routes/_main-layout.tsx
+++ src/routes/_main-layout.tsx
@@ -9,7 +9,7 @@ function MainLayout() {
     <div className="w-full">
       {/* <ThemeToggle /> */}
       {/* header */}
-      <header>
+      <header className="sticky top-0">
         <Navbar />
       </header>
       {/* main */}
