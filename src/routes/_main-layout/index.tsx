fac76ddb828a4be307221fe007a95dddcaf70d5c
Dominic Giarrusso <98626228+DomGiarrusso@users.noreply.github.com>
2026-01-18 21:04:54 -0500

    wip: initial project section

--- src/routes/_main-layout/index.tsx
+++ src/routes/_main-layout/index.tsx
@@ -1,10 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useLoaderData } from '@tanstack/react-router'
 import Hero from '@/components/home/hero'
 import Skills from '@/components/home/skills'
+import { supabase } from '@/lib/supabase'
+import Projects from '@/components/home/projects'
 
-export const Route = createFileRoute('/_main-layout/')({ component: App })
+export const Route = createFileRoute('/_main-layout/')({
+  loader: async () => {
+    const { data, error} = await supabase
+    .from('projects')
+    .select('id, title, slug, blurb, thumbnail_url')
+    .eq('is_published', true)
+    .order('sort_order', { ascending: true })
+    .order('created_at', { ascending: false })
+
+    if (error){
+      console.error("Error fetching projects", error)
+    }
+    return { projects: data ?? [] }
+  },
+  component: App })
 
 function App() {
+
+  const { projects } = Route.useLoaderData()
+
   return (
     <>
       {/* Hero */}
@@ -12,7 +31,7 @@ function App() {
       {/* Skills */}
       <Skills />
       {/* Projects */}
-
+      <Projects projects={projects} />
       {/* About */}
 
       {/* <ComponentExample /> */}
