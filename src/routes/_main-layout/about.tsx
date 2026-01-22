0699d10f4fcfcaf325b574d08617036e3fd6ea6f
Dominic Giarrusso <98626228+DomGiarrusso@users.noreply.github.com>
2026-01-21 22:39:44 -0500

    Revert "feat(page): create initial homepage" (#3)
    This reverts commit 8173ecdd487f55e2781eaa729e4f0d3fa8a7e6d9.

--- src/routes/_main-layout/about.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_main-layout/about')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_main-layout/about"! My name is Dominic </div>
-}
