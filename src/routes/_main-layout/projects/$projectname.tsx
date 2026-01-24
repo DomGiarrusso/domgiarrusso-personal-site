567b2c38f4812ef7b70485bcf9c79b91ec42ce76
Dominic Giarrusso <98626228+DomGiarrusso@users.noreply.github.com>
2026-01-24 15:14:39 -0500

    wip: initial project carousel

--- src/routes/_main-layout/projects/$projectname.tsx
+++ src/routes/_main-layout/projects/$projectname.tsx
@@ -1,3 +1,8 @@
+import { SkillConfig } from "@/components/home/skills-card";
+import ProjectCarousel from "@/components/projects/project-carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import DynamicSkillBadgeList from "@/components/ui/dynamic-skill-badge-list";
+import { getSkills } from "@/lib/skills-registry";
 import { supabase } from "@/lib/supabase";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -21,9 +26,24 @@ export const Route = createFileRoute('/_main-layout/projects/$projectname')({
 
 function ProjectPage() {
     const { project } = Route.useLoaderData();
+
+    const techStack: SkillConfig[] = getSkills(project?.techstack ?? []);
     return (
         <div>
-            <h1>{project?.title}</h1>
+            <h1 className="text-5xl font-bold text-center mt-12 mb-6">{project?.title}</h1>
+            <DynamicSkillBadgeList skills={techStack} />
+            <div className="flex flex-col md:flex-row gap-6 mt-20">
+                <div className="flex-1 space-y-4">
+                    <ProjectCarousel />
+                </div>
+                <div className="flex-1 space-y-4 pl-8">
+                    <Card className="min-h-96">
+                        <CardContent>
+                            Col 2
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
