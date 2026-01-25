13256d1bf2437c053d04e4cd05ff0ce5efce201f
Dominic Giarrusso <98626228+DomGiarrusso@users.noreply.github.com>
2026-01-24 22:39:59 -0500

    wip: initial markdown setup

--- src/routes/_main-layout/projects/$projectname.tsx
+++ src/routes/_main-layout/projects/$projectname.tsx
@@ -1,10 +1,15 @@
 import { SkillConfig } from "@/components/home/skills-card";
 import ProjectCarousel from "@/components/projects/project-carousel";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import DynamicSkillBadgeList from "@/components/ui/dynamic-skill-badge-list";
 import { getSkills } from "@/lib/skills-registry";
 import { supabase } from "@/lib/supabase";
 import { createFileRoute } from "@tanstack/react-router";
+import MarkdownContent from "@/components/ui/MarkdownContent";
+import { GitHubIcon } from "@/components/icons/tools-icons";
+import { Button } from "@/components/ui/button";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { LinkSquare02Icon } from "@hugeicons/core-free-icons";
 
 
 export const Route = createFileRoute('/_main-layout/projects/$projectname')({
@@ -15,7 +20,7 @@ export const Route = createFileRoute('/_main-layout/projects/$projectname')({
         .select('*')
         .eq('slug', projectname)
         .single()
-
+        
         if (error) {
             console.error("Error fetching project", error)
         }
@@ -25,9 +30,11 @@ export const Route = createFileRoute('/_main-layout/projects/$projectname')({
 });
 
 function ProjectPage() {
+    
     const { project } = Route.useLoaderData();
 
     const techStack: SkillConfig[] = getSkills(project?.techstack ?? []);
+
     return (
         <div>
             <h1 className="text-5xl font-bold text-center mt-12 mb-6">{project?.title}</h1>
@@ -38,9 +45,26 @@ function ProjectPage() {
                 </div>
                 <div className="flex-1 space-y-4 pl-8">
                     <Card className="min-h-96">
-                        <CardContent>
-                            Col 2
+                        <CardContent >
+                            <MarkdownContent>{project?.content}</MarkdownContent>
                         </CardContent>
+                        <CardFooter className="flex gap-2">
+                            {project?.repo_url && 
+                            <a href={project.repo_url} target="_blank" rel="noopener noreferrer">
+                                <Button variant="outline" className="cursor-pointer">
+                                    <GitHubIcon className="size-4" />
+                                    View Repo
+                                    <HugeiconsIcon icon={LinkSquare02Icon} strokeWidth={2} className="size-4" />
+                                </Button>
+                            </a>}
+                            {project?.external_url && 
+                            <a href={project.external_url} target="_blank" rel="noopener noreferrer">
+                                <Button variant="outline" className="cursor-pointer">
+                                    View Project
+                                    <HugeiconsIcon icon={LinkSquare02Icon} strokeWidth={2} className="size-4" />
+                                </Button>
+                            </a>}
+                        </CardFooter>
                     </Card>
                 </div>
             </div>
