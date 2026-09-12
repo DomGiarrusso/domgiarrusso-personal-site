f49adca942031dae2f2ae5140078fd8cabf77b6a
Dominic Giarrusso <98626228+DomGiarrusso@users.noreply.github.com>
2026-09-12 16:42:46 -0400

    feat(about): update about section and career timeline

--- src/components/home/about-me.tsx
+++ src/components/home/about-me.tsx
@@ -23,17 +23,31 @@ export default function AboutMe() {
             </CardTitle>
           </CardHeader>
           <CardContent className="text-sm leading-7 sm:text-base">
-            I’m a Full-Stack Developer at CACI Inc., where I work on diverse
-            projects across the stack. My passion for programming began during
-            my time at George Mason University, where I studied Computer Game
-            Design. With foundations in C/C++ and C#, I honed my skills through
-            projects like my capstone, Awakened, built with Unreal Engine 5.
-            Since graduating, I’ve broadened my expertise to include software
-            and web development, mastering tools like Python and JavaScript.
-            Along the way, I’ve developed a knack for teaching myself, adapting
-            to challenges, and exploring new technologies. My journey is far
-            from over, and I’m excited to continue growing, building, and
-            creating innovative solutions.
+            Well, I am a software developer who enjoys working across the stack,
+            AKA Fullstack Developer. I primarily work with C#/.NET, JS/TS, and
+            SQL. I mostly do web development nowadays, but I can and have done
+            CLIs, desktop, and game development. Outside of dev work, I enjoy
+            doing photography, making videos, and tinkering. Bonus… I am a big
+            typography nerd, too. Actually, I am a nerd of many things: Fantasy,
+            Sci-Fi, Anime, Comics, etc etc. You name it, I probably know it or
+            have at least heard of it. It kinda comes with being a developer.
+            <br />
+            <br />
+            A bit about my background: I went to George Mason University. I
+            earned my degree in Computer Game Design and then pivoted right into
+            web development. (I know, pretty obvious progression ??) Though,
+            through my degree, I developed a deep passion for creating and
+            expressing that through programming. I will say the biggest skill I
+            honed through college was learning how to learn, aka
+            problem-solving. Nowadays, when I don’t know something, it just
+            means it is a challenge for me to figure it out.
+            <br />
+            <br />
+            So that leaves me with this. First, wow, you read this far! I must
+            say, I am impressed. You must really want to get to know me. Well, I
+            can’t put down everything, nor should I. Though, if you are curious
+            and want to know more, here are some options: the rest of this site,
+            duhhh, or click the contact found in the nav and send me a message!
           </CardContent>
         </Card>
 
@@ -42,19 +56,39 @@ export default function AboutMe() {
             <TimelineHeading>Fullstack Developer</TimelineHeading>
             <TimelineDot status="custom" />
             <TimelineLine done />
-            <TimelineContent>Timeline Content</TimelineContent>
+            <TimelineContent>
+              <span className="block font-medium text-foreground">
+                2023 - Present
+              </span>
+              Building and maintaining web applications across the stack with
+              .NET/C#, JavaScript, and SQL.
+            </TimelineContent>
           </TimelineItem>
           <TimelineItem>
             <TimelineHeading>Game Instructor</TimelineHeading>
             <TimelineDot status="done" />
             <TimelineLine done />
-            <TimelineContent>Timeline Content</TimelineContent>
+            <TimelineContent>
+              <span className="block font-medium text-foreground">
+                2022 - 2023
+              </span>
+              Guiding students through game development while helping them build
+              their programming, design, and problem-solving skills.
+            </TimelineContent>
           </TimelineItem>
           <TimelineItem>
-            <TimelineHeading>Bachelors in Computer Game Design</TimelineHeading>
+            <TimelineHeading>
+              Bachelor's in Computer Game Design
+            </TimelineHeading>
             <TimelineDot status="done" />
             <TimelineLine done />
-            <TimelineContent>Timeline Content</TimelineContent>
+            <TimelineContent>
+              <span className="block font-medium text-foreground">
+                Graduated 2023
+              </span>
+              Studied game design and development, building a foundation in
+              programming, interactive design, and creative problem-solving.
+            </TimelineContent>
           </TimelineItem>
         </Timeline>
       </div>
