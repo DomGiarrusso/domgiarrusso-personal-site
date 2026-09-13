ca302280f013bd7161a506d45dbf3b69a1d41d30
Dominic Giarrusso <98626228+DomGiarrusso@users.noreply.github.com>
2026-09-13 11:59:12 -0400

    chore: update dependencies and development toolchain 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import mdx from '@mdx-js/rollup'
@@ -24,10 +23,6 @@ const config = defineConfig({
     devtools(),
     tanstackStart(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     mdx({
       remarkPlugins: [remarkGfm],
@@ -35,6 +30,9 @@ const config = defineConfig({
     }),
     viteReact(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 })
 
 export default config
